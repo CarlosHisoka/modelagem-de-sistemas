e58c2b3021ab28c21b5375d837d854e97e8db854
CarlosHisoka <129615108+CarlosHisoka@users.noreply.github.com>
2025-06-16 17:36:02 -0300

    Update banco.sql

--- scripts/banco.sql
+++ scripts/banco.sql
@@ -1,1 +1,26 @@
+CREATE TABLE `animais` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `nome` varchar(255),
+  `idade` integer,
+  `peso` float,
+  `exames` text,
+  `proprietario_id` integer
+);
 
+CREATE TABLE `proprietarios` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `nome` varchar(255),
+  `endereco` text,
+  `telefone` varchar(255)
+);
+
+CREATE TABLE `agendamentos` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `animal_id` integer,
+  `data_agendamento` timestamp,
+  `status` varchar(255)
+);
+
+ALTER TABLE `animais` ADD FOREIGN KEY (`proprietario_id`) REFERENCES `proprietarios` (`id`);
+
+ALTER TABLE `agendamentos` ADD FOREIGN KEY (`animal_id`) REFERENCES `animais` (`id`);
