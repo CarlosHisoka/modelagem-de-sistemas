35af2596fae16e0ac4a24596e79fb656e3e74a11
CarlosHisoka <129615108+CarlosHisoka@users.noreply.github.com>
2025-06-16 17:44:48 -0300

    Update crud.py

--- src/crud.py
+++ src/crud.py
@@ -1,1 +1,28 @@
+import mysql.connector
 
+# Conexão com o MySQL
+conexao = mysql.connector.connect(
+    host="localhost",
+    user="admin",
+    password="admin",
+    database="castracao_animal"
+)
+cursor = conexao.cursor()
+
+# Função para cadastrar animal
+def cadastrar_animal():
+    nome = input("Nome do animal: ")
+    idade = int(input("Idade: "))
+    peso = float(input("Peso: "))
+    cursor.execute("INSERT INTO animais (nome, idade, peso) VALUES (%s, %s, %s)", (nome, idade, peso))
+    conexao.commit()
+    print("Animal cadastrado!")
+
+# Menu interativo
+while True:
+    print("\n1 - Cadastrar animal\n2 - Sair")
+    opcao = input("Escolha: ")
+    if opcao == "1":
+        cadastrar_animal()
+    else:
+        break
